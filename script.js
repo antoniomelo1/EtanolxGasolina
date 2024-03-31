c36a01ccb51f70685abc640bee43828327a5aa8a
antoniomelo1 <antoniorodriguesmelo200@gmail.com>
2024-03-31 10:52:38 -0300

    Adicionado imagem e renderisação de cores

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function calcular() {
+/*function calcular() {
   var etanol = parseFloat(document.getElementById('etanol').value);
   var gasolina = parseFloat(document.getElementById('gasolina').value);
   var div = etanol / gasolina;
@@ -11,4 +11,24 @@ function calcular() {
   } else {
       document.getElementById('resultado').innerText = 'Tanto faz Gasolina ou Etanol!';
   }
-}
+}*/
+
+function calcular() {
+    var etanol = parseFloat(document.getElementById('etanol').value);
+    var gasolina = parseFloat(document.getElementById('gasolina').value);
+    var div = etanol / gasolina;
+    var resultado = div.toFixed(3);
+    var resultadoElement = document.getElementById('resultado');
+  
+    if (resultado <= 0.699) {
+      resultadoElement.innerText = 'Abasteça com Etanol!';
+      resultadoElement.className = 'resultado resultado-etanol';
+    } else if (resultado >= 0.701) {
+      resultadoElement.innerText = 'Abasteça com Gasolina!';
+      resultadoElement.className = 'resultado resultado-gasolina';
+    } else {
+      resultadoElement.innerText = 'Tanto faz Gasolina ou Etanol!';
+      resultadoElement.className = 'resultado resultado-igual';
+    }
+  }
+  
